2106b4cac97cad3d189d300d4bf35229aa7603a3
Simón <simon.ahumada@usm.cl>
2020-04-01 23:50:10 -0300

    dankmeme

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys, traceback
 TOKEN = 'Njk0NjkxOTg2ODU0MTE3NDk2.XoPU5Q._QwabIi0FAtfb_ntoVylyHKdPNw'
 bot = commands.Bot(command_prefix='uwu ')
 bot.remove_command('help')
-initial_extensions = ['comandos.play','comandos.text','comandos.age']
+initial_extensions = ['comandos.play','comandos.text','comandos.age','comandos.memes']
 
 if __name__ == '__main__':
 	for extension in initial_extensions:
